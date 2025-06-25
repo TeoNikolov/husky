4d65dd22de6d748437d1e801aa4088423d85ae94
TeoNikolov <tnikolov@hotmail.com>
2025-06-25 13:12:04 +0200

    Add basic husky pic gen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    function getRandomHuskyImage() {
+        fetch('https://dog.ceo/api/breed/husky/images/random')
+            .then(response => response.json())
+            .then(data => {
+                const imageElement = document.getElementById('image-frame') as HTMLImageElement;
+                imageElement.src = data.message;
+            })
+            .catch(error => {
+                const errorElement = document.getElementById('error-message') as HTMLDivElement;
+                errorElement.textContent = 'Failed to load image: ' + error.message;
+                console.error('Error fetching image:', error);
+            });
+    }
+</script>
+
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-50 space-y-6">
+    <div class="text-left mb-12">
+        <h1 class="text-3xl text-gray-800 mb-8"><span class="font-bold">Welcome!</span>
+            <br>
+            This is a page about all-things-husky.
+        </h1>
+        <h2 class="text-2xl text-gray-700">Currently you can only generate random husky pics.</h2>
+    </div>
+
+    <div id="image-container" class="w-[30em] h-[30em] p-4 flex items-center justify-center border-4 border-gray-500 rounded-4xl shadow-lg bg-gray-100">
+        <img id="image-frame" class="w-full h-full object-contain" src="https://raw.githubusercontent.com/TeoNikolov/husky/refs/heads/main/husky.webp" alt="Husky" />
+        <div id="error-message" style="color: red;"></div>
+    </div>
+
+    <button
+        on:click={getRandomHuskyImage}
+        class="bg-blue-500 hover:bg-blue-600 active:bg-blue-900 text-white font-semibold px-6 py-2 rounded-2xl shadow-lg transition"
+    >
+        New Husky
+    </button>
+    <div class="mt-12 space-y-4">
+        <p class="text-lg text-gray-600">Images are generated from <a class="font-bold" href="https://dog.ceo/dog-api/">https://dog.ceo/dog-api/</a>.</p>            
+        <p class="text-lg text-gray-600">Source code: <a class="font-bold" href="https://github.com/teonikolov/husky">https://github.com/teonikolov/husky</a></p>
+    </div>
+</div>
